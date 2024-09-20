ef3aa421e73fee6dbe9bc78694b55db5952f6a3a
Holger Knoche <hkn@informatik.uni-kiel.de>
2024-09-20 14:24:30 +0200

    Added some Javadoc

--- src/main/java/gutta/prediction/event/EntityWriteEvent.java
+++ src/main/java/gutta/prediction/event/EntityWriteEvent.java
@@ -1,5 +1,14 @@
 package gutta.prediction.event;
 
+/**
+ * This event represents a write access to a specific data entity.
+ * 
+ * @param traceId The id of the trace containing this event
+ * @param timestamp The timestamp at which this event occurred
+ * @param location The location at which this timestamp occurred
+ * @param entityType The written entity type's name
+ * @param entityIdentifier The id of the written entity
+ */
 public record EntityWriteEvent(long traceId, long timestamp, Location location, String entityType, String entityIdentifier) implements MonitoringEvent {
 
     @Override
--- src/main/java/gutta/prediction/event/MonitoringEvent.java
+++ src/main/java/gutta/prediction/event/MonitoringEvent.java
@@ -1,17 +1,38 @@
 package gutta.prediction.event;
 
+/**
+ * A {@link MonitoringEvent} represents a meaningful event in a observed or rewritten {@link EventTrace}.
+ */
 public interface MonitoringEvent {
 
+    /**
+     * The ID of the trace in which this event occurred.
+     * 
+     * @return see above
+     */
     long traceId();
 
+    /**
+     * The timestamp (in nanoseconds) at which this event occurred.
+     * 
+     * @return see above
+     */
     long timestamp();
 
+    /**
+     * The location at which this event occurred.
+     * 
+     * @return see above
+     */
     Location location();
-
-    default boolean isSynthetic() {
-        return false;
-    }
     
+    /**
+     * Accepts the given visitor.
+     * 
+     * @param <R> The type of the operation provided by the visitor 
+     * @param visitor The visitor to accept
+     * @return The result of the operation provided by the visitor
+     */
     <R> R accept(MonitoringEventVisitor<R> visitor);
 
 }
