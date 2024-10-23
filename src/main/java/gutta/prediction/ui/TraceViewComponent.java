364d5f842eb594a9fbb104b470dcbd9dca0519da
Holger Knoche <hkn@informatik.uni-kiel.de>
2024-10-23 16:13:08 +0200

    Added option to save to svg

--- src/main/java/gutta/prediction/ui/TraceViewComponent.java
+++ src/main/java/gutta/prediction/ui/TraceViewComponent.java
@@ -1,15 +1,32 @@
 package gutta.prediction.ui;
 
 import gutta.prediction.span.Trace;
+import org.apache.batik.dom.GenericDOMImplementation;
+import org.apache.batik.svggen.SVGGraphics2D;
+import org.w3c.dom.DOMImplementation;
+import org.w3c.dom.Document;
 
 import java.awt.Dimension;
+import java.awt.FileDialog;
+import java.awt.Frame;
 import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
+import javax.swing.JMenuItem;
+import javax.swing.JPopupMenu;
 
 class TraceViewComponent extends TraceComponent {
 
     private static final long serialVersionUID = 29828340270697690L;
+    
+    private static final String SVG_NAMESPACE = "http://www.w3.org/2000/svg";
     
     private static final int BORDER_WIDTH = 20;
     
@@ -19,7 +36,23 @@ class TraceViewComponent extends TraceComponent {
                 
     private Dimension preferredSize = DEFAULT_DIMENSION;
     
+    private InitializeOnce<JPopupMenu> popupMenu = new InitializeOnce<>(this::createPopupMenu);
+    
     private List<DrawableShape> shapes = new ArrayList<>();
+    
+    public TraceViewComponent() {
+        this.setComponentPopupMenu(this.popupMenu.get());
+    }
+    
+    private JPopupMenu createPopupMenu() {
+        var menu = new JPopupMenu();
+        
+        var saveToSvgItem = new JMenuItem("Save to SVG...");
+        saveToSvgItem.addActionListener(this::saveToSvgAction);
+        menu.add(saveToSvgItem);
+        
+        return menu;
+    }
     
     public void trace(Trace trace) {
         if (trace == null) {
@@ -56,6 +89,39 @@ class TraceViewComponent extends TraceComponent {
     @Override
     protected void paint(Graphics2D graphics) {
         this.shapes.forEach(shape -> shape.drawOn(graphics));
-    }        
+    }    
+   
+    private void saveToSvgAction(ActionEvent event) {
+        var optionalFile = this.loadFileWithDialog();
+        
+        optionalFile.ifPresent(this::saveToSvg);
+    }
+    
+    private Optional<File> loadFileWithDialog() {
+        var openDialog = new FileDialog((Frame) null);
+        openDialog.setMultipleMode(false);
+        openDialog.setVisible(true);
+
+        var selectedFiles = openDialog.getFiles();
+        if (selectedFiles.length == 0) {
+            return Optional.empty();
+        } else {
+            return Optional.of(selectedFiles[0]);            
+        }
+    }
+    
+    private void saveToSvg(File file) {
+        DOMImplementation domImplementation = GenericDOMImplementation.getDOMImplementation();
+        Document document = domImplementation.createDocument(SVG_NAMESPACE, "svg", null);
+        SVGGraphics2D svgGraphics = new SVGGraphics2D(document);
+
+        this.paint(svgGraphics);
+
+        try (Writer writer = new FileWriter(file)) {
+            svgGraphics.stream(writer, true);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
     
 }
