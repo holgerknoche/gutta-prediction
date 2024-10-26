a6bfb8f6f7841c365508fb283fc0f9508a15e8d9
Holger Knoche <hkn@informatik.uni-kiel.de>
2024-10-26 22:08:09 +0200

    Added example trace with implicit abort

--- src/main/java/gutta/prediction/datageneration/TracesWithConsistencyIssuesGenerator.java
+++ src/main/java/gutta/prediction/datageneration/TracesWithConsistencyIssuesGenerator.java
@@ -6,6 +6,7 @@ import gutta.prediction.event.EntityReadEvent;
 import gutta.prediction.event.EntityWriteEvent;
 import gutta.prediction.event.EventTrace;
 import gutta.prediction.event.ExplicitTransactionAbortEvent;
+import gutta.prediction.event.ImplicitTransactionAbortEvent;
 import gutta.prediction.event.ObservedLocation;
 import gutta.prediction.event.ServiceCandidateEntryEvent;
 import gutta.prediction.event.ServiceCandidateExitEvent;
@@ -37,6 +38,7 @@ public class TracesWithConsistencyIssuesGenerator {
         var modelSpec = 
                 "Component c1 {\n" +
                 "    UseCase \"Consistency Issues\"\n" +
+                "    ServiceCandidate sc2 [transactionBehavior=REQUIRES_NEW]" +
                 "}\n" +
                 "Component c2 {\n" +
                 "    ServiceCandidate sc1\n" +
@@ -62,6 +64,7 @@ public class TracesWithConsistencyIssuesGenerator {
         traces.add(this.buildTraceWithStaleRead(useCaseName, 1234));
         traces.add(this.buildTraceWithWritesInSubordinateTransaction(useCaseName, 1235));
         traces.add(this.buildTraceWithWriteConflict(useCaseName, 1236));
+        traces.add(this.buildTraceWithImplicitAbort(useCaseName, 1237));
         
         try (var outputStream = new FileOutputStream(fileName)) {
             new EventTraceEncoder().encodeTraces(traces, outputStream);
@@ -135,6 +138,31 @@ public class TracesWithConsistencyIssuesGenerator {
                 new EntityWriteEvent(traceId, timestamps.nextStep(), location2, entity2),
                 new ServiceCandidateExitEvent(traceId, timestamps.nextStep(), location2, "sc1"),
                 new ServiceCandidateReturnEvent(traceId, timestamps.nextStep(), location1, "sc1"),
+                new TransactionCommitEvent(traceId, timestamps.nextStep(), location1, "tx1"),
+                new UseCaseEndEvent(traceId, timestamps.nextStep(), location1, useCaseName)                
+                );
+    }
+    
+    private EventTrace buildTraceWithImplicitAbort(String useCaseName, long traceId) {
+        var timestamps = new TimestampGenerator(20, 0, 0);
+        
+        var location1 = new ObservedLocation("test1", 123, 1);
+        var location2 = new ObservedLocation("test2", 123, 1);
+        
+        var entityType = new EntityType("et1");
+        var entity1 = new Entity(entityType, "1");
+        var entity2 = new Entity(entityType, "2");
+        
+        return EventTrace.of(
+                new UseCaseStartEvent(traceId, timestamps.nextStep(), location1, useCaseName),
+                new TransactionStartEvent(traceId, timestamps.nextStep(), location1, "tx1"),
+                new EntityWriteEvent(traceId, timestamps.nextStep(), location1, entity1),
+                new ServiceCandidateInvocationEvent(traceId, timestamps.nextStep(), location1, "sc2"),
+                new ServiceCandidateEntryEvent(traceId, timestamps.noStep(), location2, "sc2", true, "tx2"),
+                new EntityWriteEvent(traceId, timestamps.nextStep(), location2, entity2),
+                new ImplicitTransactionAbortEvent(traceId, timestamps.nextStep(), location2, "tx2", "some error"),
+                new ServiceCandidateExitEvent(traceId, timestamps.nextStep(), location2, "sc2"),
+                new ServiceCandidateReturnEvent(traceId, timestamps.noStep(), location1, "sc2"),
                 new TransactionCommitEvent(traceId, timestamps.nextStep(), location1, "tx1"),
                 new UseCaseEndEvent(traceId, timestamps.nextStep(), location1, useCaseName)                
                 );
