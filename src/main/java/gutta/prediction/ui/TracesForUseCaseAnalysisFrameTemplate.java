b69016b1d63179ac09affe23f0333b8c31208adf
Holger Knoche <hkn@informatik.uni-kiel.de>
2024-10-27 00:00:04 +0200

    Created generic analysis frame template

---
+++ src/main/java/gutta/prediction/ui/TracesForUseCaseAnalysisFrameTemplate.java
@@ -0,0 +1,9 @@
+package gutta.prediction.ui;
+
+abstract class TracesForUseCaseAnalysisFrameTemplate <T extends Comparable<T>> extends UIFrameTemplate {
+
+    private static final long serialVersionUID = 7779060688846998337L;
+    
+    
+
+}
